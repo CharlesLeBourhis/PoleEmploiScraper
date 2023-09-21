6c83b2f3052b22a550915ba7265090ab7d46315a
CharlesLeBourhis <116649597+CharlesLeBourhis@users.noreply.github.com>
2023-09-21 17:56:39 +0200

    fixed github action

--- main.py
+++ main.py
@@ -21,27 +21,29 @@ if __name__ == "__main__":
 
         r = requests.get(f"https://candidat.pole-emploi.fr{job_link}")
 
-        soup = BeautifulSoup(r.content, "lxml")
+        if r.status_code == 200:
 
-        df_ =  pd.DataFrame.from_dict(
-            {
-                "id": job_link.split("/")[-1],
-                "url": f"https://candidat.pole-emploi.fr{job_link}",
-                "date": soup.find("p", class_="t5 title-complementary").find("span").get("content"),
-                "title": soup.title.string,
-                "location": soup.find("p", itemprop="jobLocation").find("span", itemprop="name").text,
-                "description": soup.find_all("div", class_="description")[0].p.string,
-                "info": soup.find_all("div", class_="description-aside")[0].dd.text,
-                "horaires": soup.find("div", class_="description-aside", ).find("dd", itemprop="workHours").text.strip() if soup.find("div", class_="description-aside", ).find("dd", itemprop="workHours") else "unspecified",
-                "salairemin": soup.find("div", class_="description-aside", ).find("span", itemprop="minValue").get("content") if soup.find("div", class_="description-aside", ).find("span", itemprop="minValue") else "unspecified",
-                "salairemax": soup.find("div", class_="description-aside", ).find("span", itemprop="maxValue").get("content") if soup.find("div", class_="description-aside", ).find("span", itemprop="maxValue") else "unspecified",
-                "entreprise": soup.find("h3", class_="t4 title").text.strip() if soup.find("h3", class_="t4 title") else "unspecified",
-                "experience": soup.find_all("ul", class_="skill-list list-unstyled")[0].find("span", class_="skill-name").text,
-            },
-            orient="index",
-            ).T
-        
-        df = pd.concat([df, df_], ignore_index=True)
+            soup = BeautifulSoup(r.content, "lxml")
+
+            df_ =  pd.DataFrame.from_dict(
+                {
+                    "id": job_link.split("/")[-1],
+                    "url": f"https://candidat.pole-emploi.fr{job_link}",
+                    "date": soup.find("p", class_="t5 title-complementary").find("span").get("content"),
+                    "title": soup.title.string,
+                    "location": soup.find("p", itemprop="jobLocation").find("span", itemprop="name").text,
+                    "description": soup.find_all("div", class_="description")[0].p.string,
+                    "info": soup.find_all("div", class_="description-aside")[0].dd.text,
+                    "horaires": soup.find("div", class_="description-aside", ).find("dd", itemprop="workHours").text.strip() if soup.find("div", class_="description-aside", ).find("dd", itemprop="workHours") else "unspecified",
+                    "salairemin": soup.find("div", class_="description-aside", ).find("span", itemprop="minValue").get("content") if soup.find("div", class_="description-aside", ).find("span", itemprop="minValue") else "unspecified",
+                    "salairemax": soup.find("div", class_="description-aside", ).find("span", itemprop="maxValue").get("content") if soup.find("div", class_="description-aside", ).find("span", itemprop="maxValue") else "unspecified",
+                    "entreprise": soup.find("h3", class_="t4 title").text.strip() if soup.find("h3", class_="t4 title") else "unspecified",
+                    "experience": soup.find_all("ul", class_="skill-list list-unstyled")[0].find("span", class_="skill-name").text,
+                },
+                orient="index",
+                ).T
+            
+            df = pd.concat([df, df_], ignore_index=True)
 
     df["contrat"] = df["info"].str.strip().str.split("\n\n").str[0]
     # df["apprentissage"] = df["info"].str.strip().str.split("\n\n").str[1] == "Contrat apprentissage"
